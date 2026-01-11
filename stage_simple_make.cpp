6876c1efa32772cc9f00df54cde3512f6d4910de
libelaugh <libelaugh55@gmail.com>
2026-01-11 21:56:11 +0900

    Add files via upload

--- stage_simple_make.cpp
+++ stage_simple_make.cpp
@@ -15,7 +15,7 @@
 
 using namespace DirectX;
 
-static XMFLOAT3 no{0.0f,0.0f,0.0f};
+static XMFLOAT3 no{ 0.0f,0.0f,0.0f };
 static float aTime = 0.0f;
 
 static float aPrevOffsetY = 0.0f;
@@ -32,11 +32,10 @@ void StageSimple_Update(double elapsedTime)
 {
     aTime += static_cast<float>(elapsedTime);
 
-    const float offsetY = 5.0f * sinf(aTime*0.5);
+    const float offsetY = 5.0f * sinf(aTime);
     const float deltaY = offsetY - aPrevOffsetY;
-    aPrevOffsetY = offsetY;
-    Stage01_AddObjectTransform(72, { 0.0f, deltaY, 0.0f }, no, no);
 
+    bool ridePlatform = false;
     if (std::fabs(deltaY) > 0.0f)
     {
         const StageBlock* block = Stage01_Get(72);
@@ -53,13 +52,18 @@ void StageSimple_Update(double elapsedTime)
             const XMFLOAT3& playerVel = Player_GetVelocity();
             const bool canRidePlatform = Player_IsGrounded() && (playerVel.y <= 0.01f);
 
-            if (overlapXZ && dy >= -0.002f && dy <= kGroundEps && canRidePlatform)
-            {
-                DirectX::XMFLOAT3 pos = Player_GetPosition();
-                pos.y += deltaY;
-                Player_DebugTeleport(pos, false);
-            }
+            ridePlatform = overlapXZ && dy >= -0.002f && dy <= kGroundEps && canRidePlatform;
         }
+    }
+
+    aPrevOffsetY = offsetY;
+    Stage01_AddObjectTransform(72, { 0.0f, deltaY, 0.0f }, no, no);
+
+    if (ridePlatform)
+    {
+        DirectX::XMFLOAT3 pos = Player_GetPosition();
+        pos.y += deltaY;
+        Player_DebugTeleport(pos, false);
     }
 
 }
