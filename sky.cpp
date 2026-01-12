2b1f1c1e365851eb31c01d137ab5b2bdfce19c89
libelaugh <libelaugh55@gmail.com>
2026-01-12 23:59:24 +0900

    Add files via upload

--- sky.cpp
+++ sky.cpp
@@ -8,12 +8,14 @@
 ==============================================================================*/
 
 #include "sky.h"
-#include"model.h"
-#include"shader3d_unlit.h"
+#include "direct3d.h"
+#include "model.h"
+#include "shader3d_unlit.h"
 
 using namespace DirectX;
 
 static MODEL* g_pModelSky{ nullptr };
+static ID3D11RasterizerState* g_pRasterizerStateCullNone{ nullptr };
 static XMFLOAT3 g_position{};
 
 
@@ -21,11 +23,21 @@ void Sky_Initialize()
 {
 
 	g_pModelSky = ModelLoad("sky.fbx", 100.0f, true);
+
+	ID3D11Device* device = Direct3D_GetDevice();
+	if (device) {
+		D3D11_RASTERIZER_DESC rd{};
+		rd.FillMode = D3D11_FILL_SOLID;
+		rd.CullMode = D3D11_CULL_NONE;
+		rd.DepthClipEnable = TRUE;
+		device->CreateRasterizerState(&rd, &g_pRasterizerStateCullNone);
+	}
 }
 
 void Sky_Finalize()
 {
 	ModelRelease(g_pModelSky);
+	SAFE_RELEASE(g_pRasterizerStateCullNone);
 }
 
 void Sky_Draw()
@@ -34,7 +46,19 @@ void Sky_Draw()
 
 	g_position.y = 0.0f;
 
+	ID3D11DeviceContext* ctx = Direct3D_GetContext();
+	ID3D11RasterizerState* prevState = nullptr;
+	if (ctx && g_pRasterizerStateCullNone) {
+		ctx->RSGetState(&prevState);
+		ctx->RSSetState(g_pRasterizerStateCullNone);
+	}
+
 	ModelUnlitDraw(g_pModelSky, XMMatrixTranslationFromVector(XMLoadFloat3(&g_position)));
+
+	if (ctx && g_pRasterizerStateCullNone) {
+		ctx->RSSetState(prevState);
+	}
+	SAFE_RELEASE(prevState);
 }
 
 void Sky_SetPosition(const DirectX::XMFLOAT3& position)
